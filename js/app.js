d75eaaf3f4bbea05c17a65f2c93a82094828f1f3
Diego de Jesus Bonfim <dijb988@hotmail.com>
2014-07-05 02:56:54 -0300

    Correção de Bugs
    Corrigido erro de repetição de perguntas
    Corrigido erro de avaliação no final
    Corrigido perguntas erradas

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ $(function(){
 	var respostas = [];
 	var indexPergunta = 0;
 	var dbPerguntas;
-	var perguntasRepetidas = [];
+	var perguntasRepetidas = []; /*vetor de perguntas que já sairam*/
 	var respostasPerguntas = [];
-	var countPerg = 0;
+	var countPerg = 0; 
 	var perguntasSorteadas = [];
-	var numeroPerguntas = 10;
+	var numeroPerguntas = 10; /*quantidade de perguntas por rodada*/
+	var certas=0; /*contador de certas*/
+	var vezes=0; /*numero de vezes jogada sem parar*/
 	
 	/* colocando json em variavel*/
 	$.getJSON("js/perguntas.json", function(json){
@@ -26,36 +28,28 @@ $(function(){
 		$('#alternativa-3').html("c) "+dbPerguntas[index].alternativas[2]);
 		$('#imagens').attr('src',dbPerguntas[index].imagem);
 		respostasPerguntas[countPerg] = dbPerguntas[index].resposta;
-		countPerg++;		
+		countPerg++;
 	}
 	
 	function randomInt(min, max){ /*gera o index randomico e confere se já foi usado*/
 			var index = -1;
+			var saiu = 0;
 			do {
 				index = Math.floor(Math.random()*(max-min+1))+min;
 				console.log(index);
-				for(i = 0; i < perguntasRepetidas.length; i++){
+				for (i=0; i < perguntasRepetidas.length; i++) {
 					if(perguntasRepetidas[i] == index){
-						index = -1;
-					}
-					else {
-						perguntasRepetidas.push(index);
-						break;
+						saiu++;
 					}
 				}
+				if (saiu>0){
+					index=-1;
+				}
+				saiu=0;
 			}while(index < 0)
+			perguntasRepetidas.push(index);
 			console.log(index);
 			return index;
-	}
-	
-	function correcao(){ /*compara os vetores de respostas do usuário e do JSON*/
-		var acerto=0;
-		for (i = 0; i < respostas.length; i++){
-			if (respostas[i]==respostasPerguntas[i]){
-				acerto++;
-			}
-		}
-		return acerto;
 	}
 	
 	function gabarito(){ /* criar o gabarito */
@@ -83,6 +77,7 @@ $(function(){
 			} else {
 				estilo = "";
 				suaResposta = "";
+				certas++;
 			}
 			
 			gabaritoCompleto = gabaritoCompleto + "<li "+estilo+"><p style='align:justify;'><strong>"+dbPerguntas[questao].pergunta + "</strong></p><p><strong>Resposta:</strong> " /*Concatenação do gabarito*/
@@ -93,6 +88,8 @@ $(function(){
 	
 	$("#btComecar").click(function(){ /* botão Começar chama o metodo para carregar a pergunta e zera todos os vetores*/
 		indexPergunta = 0;
+		vezes=0;
+		certas=0;
 		respostas = [];
 		perguntasRepetidas = [];
 		respostasPerguntas = [];
@@ -114,9 +111,9 @@ $(function(){
 			}
 			if (indexPergunta == numeroPerguntas){ /*mostra resultado*/
 				$.mobile.changePage( "#resultado", { transition: "flip"});
-				var num = correcao();
-				$("#redResultado").html( "Você " +(num===0 ? "não ": "")+"acertou " +(num===0 ? "nenhuma ": num) + (num <= 1 ? " questão!" : " questões!") + (num < 1 ? "</br>:( </br> Tente de Novo!</br>:D" : ""));
 				gabarito();
+				$("#redResultado").html( "Você " +(certas===0 ? "não ": "")+"acertou " +(certas===0 ? "nenhuma ": certas) + (certas <= 1 ? " questão!" : " questões!") + (certas < 1 ? "</br>:( </br> Tente de Novo!</br>:D" : ""));
+				
 			} else {
 				carregarPergunta();
 				$("input[name='radio-choice-v-2']").attr("checked", false);
@@ -132,10 +129,15 @@ $(function(){
 	});
 	
 	$("#btContinua").click(function(){ /*inicia o processo de geração de perguntas*/
+		vezes++;
+		certas=0;
 		indexPergunta = 0;
 		respostas = [];
 		respostasPerguntas = [];
-		perguntasSorteadas = [];		
+		perguntasSorteadas = [];
+		if (vezes==3){
+			perguntasRepetidas = [];
+		}
 		carregarPergunta();
 		$("#btProxima").html('Próxima');
 		$('#formulario').each (function(){ /*apaga a marcação do radio buttom*/
